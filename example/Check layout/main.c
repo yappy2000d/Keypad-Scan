a004209fd51f5e9557e3696b284e30ca9cc1005f
LSweetSour <61788097+yappy2000d@users.noreply.github.com>
2023-11-27 08:47:35 +0800

    Update main.c
    修復檔案內容錯誤

--- example/Check layout/main.c
+++ example/Check layout/main.c
@@ -1,49 +1,19 @@
 #include <8051.h>
 #include <stdbool.h>
-//define KEY_PORT P0				// Optional
-#include "keypadlib.c"
+//define KEY_PORT P0
+#include "lib/keypadlib.c"
+#include "lib/uart.c"
 
-#define Byte unsigned char
-#define LED_PORT P1
-#define BUTTON P3_2
-#define BTN_UP 1
-
-const char KeyCodes[] = "0123456789ABCDEF";	// Optional  
-
-// concat high digits and low digits to a byte
-Byte concatByte(char a, char b) {
-  a = ascii2Byte(a);
-  b = ascii2Byte(b);
-  return (a << 4) + b;
-}
+const char KeyCodes[] = "0123456789ABCDEF";
 
 void main(void) {
-  // a: high digits, b: low digits
-  char a, b;
-  Byte summand, addend, sum;
-  
-  // key_setcodes(KeyCodes);   // Optional
-  
-  
+  char key;
+
+  key_setKeys(KeyCodes);
+  uart_initialize();
+
   while(true) {
-    a = key_getch();
-    b = key_getch();
-    
-    summand = concatByte(a, b);
-    LED_PORT = ~summand;
-    
-    // wait until button is pressed
-    while(BUTTON == BTN_UP);
-    
-    a = key_getch();
-    b = key_getch();
-    
-    addend = concatByte(a, b);
-    LED_PORT = ~addend;
-    
-    // wait until button is pressed
-    while(BUTTON == BTN_UP);
-    sum = summand + addend;
-    LED_PORT = sum;
+    key = key_getChar();
+    uart_put_char(key);
   }
 }
